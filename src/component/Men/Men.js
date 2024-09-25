12ec64d7f5424f151737056dc198d3e417602abc
Challmejosh <134891799+Challmejosh@users.noreply.github.com>
2024-09-25 14:47:16 +0100

    Update Men.js

--- src/component/Men/Men.js
+++ src/component/Men/Men.js
@@ -1,11 +1,11 @@
 import ItemList from "../itemList/ItemList";
 
-const Men = ({product,like,love,confirm}) => {
+const Men = ({product,like,love,confirm,handleAddToCart}) => {
     return ( 
         <div className="">
             {confirm ? 
         (
-            <ItemList product={product} like={like} love={love} confirm={confirm} />
+            <ItemList product={product} like={like} love={love} confirm={confirm} handleAddToCart={handleAddToCart}/>
         ) : (
             <div className="flex items-center justify-center">
                 <div className="loader"></div>
